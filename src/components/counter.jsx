dbb8be0877e1c6a4f17f4e87668f8cc5c8944722
junaid-at-092026438222 <oKhkolg8boM4SvOhg3+KbLMd39jkHadSAOmWkhmCeT4=>
2023-03-25 04:09:51 +0500

    Conditional Rendering in react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,23 +3,57 @@ import React, { Component, Fragment } from 'react'
 class Counter extends Component {
     
     state = {
-        count: 1,
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3'],
+    }
+    
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>
+
+        return (
+            <ul className='ml-3 font-bold'>
+                {
+                    this.state.tags.map(tag => <li key={tag}> { tag } </li>)
+                }
+            </ul>
+        ) 
     }
 
     render() { 
+
+        // const {tags} = this.state
+
         return (
             <Fragment>
-                <h1 className='text-red-500'>{this.formatCount()}</h1>
-                <button>Increment</button>
+                {/* <div className='flex m-3'>
+                    <span className={this.state.count === 0 ? 'bg-yellow-500 rounded-md mr-3 p-2 text-sm text-center' : 'bg-blue-500 rounded-md mr-3 p-2 text-sm text-center text-white'}>
+                        {this.formatCount()}
+                    </span>
+
+                    <button>Increment</button>
+
+                </div> */}
+
+                <div>
+                    {this.state.tags.length === 0 && 'Please create a new tag!'}
+
+                    {this.renderTags()}
+                </div>
+            
+                {/* <ul className='ml-3 font-bold'>
+                    {
+                        tags.map(tag => <li key={tag}> { tag } </li>)
+                    }
+                </ul> */}
             </Fragment>
         );
     }
+    
+    // formatCount = () => {
+    //     const { count } = this.state // destructuring the count value
 
-    formatCount = () => {
-        const { count } = this.state // destructuring the count value
-
-        return count === 0 ? <h1>Zero</h1> : count;
-    }
+    //     return count === 0 ? <h1>Zero</h1> : count;
+    // }
 }
  
 export default Counter;
